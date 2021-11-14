6756185c19fd770a775b391595ce4ca805edd162
sreeks87 <14892516+sreeks87@users.noreply.github.com>
2021-11-14 17:41:51 +0530

    tests and mocks

--- pageinfo/delivery/http/webPageInfoController_test.go
+++ pageinfo/delivery/http/webPageInfoController_test.go
@@ -2,11 +2,14 @@ package delivery_test
 
 import (
 	"bytes"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	delivery "github.com/sreeks87/webpageinfo/pageinfo/delivery/http"
+	"github.com/sreeks87/webpageinfo/pageinfo/domain"
+	"github.com/sreeks87/webpageinfo/pageinfo/service/mocks"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -61,7 +64,44 @@ func TestWebPageInfoEmptyReq3(t *testing.T) {
 }
 
 func TestWebPageInfoEmptyReq4(t *testing.T) {
+	mockSvc := new(mocks.Service)
+	p := domain.Pageinfo{
+		HTMLVersion: "4",
+		PageTitle:   "title",
+		HeadingData: domain.Head{
+			H1: 1,
+			H2: 2,
+			H3: 3,
+			H4: 4,
+			H5: 5,
+			H6: 6,
+		},
+		LinkData: domain.Links{
+			InternalLinks:     1,
+			ExternalLinks:     2,
+			InaccessibleLinks: 3,
+		},
+		LoginForm: false,
+		Error:     "",
+	}
+	mockSvc.On("Extract").Return(p, nil)
 	r := []byte(`{"url":"http://www.example.com"}`)
+	req, err := http.NewRequest("POST", "/webpageinfo", bytes.NewBuffer(r))
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(delivery.Webpageinfo)
+	handler.ServeHTTP(rr, req)
+	// Check the status code is what we expect.
+	assert.Equal(t, rr.Code, http.StatusOK)
+}
+
+func TestWebPageInfoEmptyReq5(t *testing.T) {
+	mockSvc := new(mocks.Service)
+	p := domain.Pageinfo{}
+	mockSvc.On("mockSvc.Extract").Return(p, errors.New("error"))
+	r := []byte(`{"url":"1234"}`)
 	req, err := http.NewRequest("POST", "/webpageinfo", bytes.NewBuffer(r))
 	if err != nil {
 		t.Fatal(err)
